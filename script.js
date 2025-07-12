c23959e6c752fa6459d4800e1044ac36c844f52a
Jameson Brown <144629151+avo-doggo@users.noreply.github.com>
2025-07-12 02:26:52 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const name= "Jameson Brown";
-const major= "Game Art & Animation and Game Programming";
-const email= "marbrow1@uat.edu";
-const gradDate= "May 2026";
-document.getElementById("studentName").textContent = name;
-document.getElementById("studentMajor").textContent = major;
-document.getElementById("studentEmail").textContent = email;
-document.getElementById("studentGrad").textContent = gradDate;
