84fb6efa693e8e63e636cf85209d79cd100f6678
Jameson Brown <144629151+avo-doggo@users.noreply.github.com>
2025-07-12 02:26:05 -0500

    Add files via upload

---
+++ week1/script.js
@@ -0,0 +1,8 @@
+const name= "Jameson Brown";
+const major= "Game Art & Animation and Game Programming";
+const email= "marbrow1@uat.edu";
+const gradDate= "May 2026";
+document.getElementById("studentName").textContent = name;
+document.getElementById("studentMajor").textContent = major;
+document.getElementById("studentEmail").textContent = email;
+document.getElementById("studentGrad").textContent = gradDate;
